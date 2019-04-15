5340577262b05fcf439943542ffdfada0e6a9e24
Marin <marin.procureur@gmail.com>
2019-04-15 10:12:23 +0200

    Update script.js

--- javascript/lesson5/exercise4/exercise/script.js
+++ javascript/lesson5/exercise4/exercise/script.js
@@ -1,15 +1,1 @@
 
-(function() {
-    // Immediately Invoked Function Expression
-    var items = ["Riri","Fifi","Loulou","Donald"];
-
-    var ul = document.createElement('ul');
-    for (var i in items) {
-        var li = document.createElement('li');
-        li.textContent = items[i];
-        ul.appendChild(li);
-    }
-    
-    document.body.appendChild(ul);
-
-})();
