4356f341f830829f92f0fec293a63062419e69be
Marin <marin.procureur@gmail.com>
2019-04-15 10:12:58 +0200

    Update script.js

--- javascript/lesson5/exercise2/exercise/script.js
+++ javascript/lesson5/exercise2/exercise/script.js
@@ -1,16 +1,1 @@
-var div = document.createElement("div");
-document.body.appendChild(div);
 
-function divStyle() {
-    div.style.width = "200px";
-    div.style.height = "300px";
-    div.style.backgroundColor = "blue";
-    div.onmouseover = function() {
-        div.style.backgroundColor = "red";
-    }
-    div.onmouseout = function() {
-        div.style.backgroundColor = "blue";
-    }
-}
-
-divStyle();
